d26e1870b6f0986193f742e087cce64087ded67d
RahulGautamSingh <rahultesnik@gmail.com>
2022-01-08 20:07:55 +0545

    inserting icon using vanilla

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const iconClassName = "bi bi-suit-heart-fill";
 function goToThisLink() {
-  window.open("http://google.com", "_blank");
+  window.open("https://google.com", "_blank");
 }
+function createIcon() {
+  let icon = document.createElement("i");
+  iconClassName.split(" ").map((elem) => {
+    icon.classList.add(elem);
+  });
+
+  icon.addEventListener("click", goToThisLink);
+  return icon;
+}
+
+let icon = createIcon();
+document.body.appendChild(icon);
